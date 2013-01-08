f32446c226b2e72c45c7837749211c84be3b9b74
John Schimmel <john.schimmel@gmail.com>
2013-01-08 13:51:18 -0500

    README.md for express simplewww

---
+++ http_expressjs/simplewww/README.md
@@ -0,0 +1,33 @@
+# Simple ExpressJS example
+
+## What it is
+
+ExpressJS is a microframework for NodeJS. It is very similiar to Ruby's Sinatra and Python's Flask. Express can help you get a basic HTTP web server running but also make organizing your code and design with templates, manage user sessions, connect to databases and provide authentication. 
+
+While ExpressJS does not do everything, it is great for the small apps that need very basic functionality. 
+
+## USAGE
+
+### First things first
+
+First time running the code you need to install the required modules that are defined in **package.json**. Navigate to the code directory in Terminal. Install all dependencies by running the following command.
+	
+	npm install
+
+You only need to run this code once, every time you update **package.json**.
+
+### Start the server
+
+Navigate to code directory in Terminal: 
+	
+	node app.js
+
+
+### View the webpages
+
+Open Web Browser and navigate to [http://localhost:3000](http://localhost:3000)
+
+Available URLs 
+
+* [http://localhost:3000/](http://localhost:3000)
+* [http://localhost:3000/form](http://localhost:3000)
--- http_expressjs/simplewww/app.js
+++ http_expressjs/simplewww/app.js
@@ -1,12 +1,28 @@
+/* 
 
-/**
- * Module dependencies.
- */
+Simple ExpressJS example
+* Starts a web server
+* Accepts client connection and returns "Hello World"
+--------------------------------
+USAGE
+
+First time running the code, navigate to the code directory in Terminal. Install all dependencies.
+> npm install
+
+Start Server, navigate to code directory in Terminal: 
+> node app.js
+
+Open Web Browser and navigate to http://localhost:3000 or http://127.0.0.1:3000
+
+Available urls '/'  &  '/form'
+
+*/
 
 var express = require('express');
 
 var app = express();
 
+// ExpressJS configuration
 app.configure(function(){
   app.set('port', process.env.PORT || 3000);
   app.use(express.bodyParser());
@@ -16,11 +32,15 @@ app.configure(function(){
 app.configure('development', function(){
   app.use(express.errorHandler());
 });
+// end ExpressJS config
 
+
+// display the main / index page
 app.get('/', function(req, res) {
   res.send("<h1>Hello World</h1>");
 });
 
+// display a simple form
 app.get('/form', function(req, res) {
 
 	form = "<form method='POST'><input type='text' name='mydata' placeholder='Enter some text'><input type='submit'></form>"
@@ -28,17 +48,18 @@ app.get('/form', function(req, res) {
 
 });
 
+// POST request to /form 
+// accepts a form submission receives form field 'mydata' displays it to browser
 app.post('/form', function(req, res){
 
 	console.log("form submission");
 	console.log(req.body.mydata); // form data pass in req.body 
 
-	res.send("Thanks for this: <b>" + req.body.mydata + "<b>");
+	res.send("Thanks for submitting, you said, <b>" + req.body.mydata + "<b>");
 
 	
 });
 
-//app.get('/users', user.list);
-
+// start the server!
 app.listen(app.get('port'));
 console.log("Server started on port "+ app.get('port'));
